6fa97f65a29193c8ec9ee8bb97de5bae6194cdcf
Dominic Jodoin <cotsog@gmail.com>
2017-02-21 11:23:26 -0500

    Only delete stale files starting with packagecloud.io

--- lib/travis/build/appliances/fix_rwky_redis.rb
+++ lib/travis/build/appliances/fix_rwky_redis.rb
@@ -6,7 +6,7 @@ module Travis
       class FixRwkyRedis < Base
         def apply
           command = <<-EOF
-            sudo rm -rf /var/lib/apt/lists/*
+            sudo rm -rf /var/lib/apt/lists/packagecloud.io*
             for f in $(grep -l rwky/redis /etc/apt/sources.list.d/*); do
               sed 's,rwky/redis,rwky/ppa,g' $f > /tmp/${f##**/}
               sudo mv /tmp/${f##**/} /etc/apt/sources.list.d
